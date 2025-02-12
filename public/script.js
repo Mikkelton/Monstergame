83e9a75683b421fca3309c795c8e358324d525f4
Mikkelton <mikaelhakkinen7@gmail.com>
2025-02-12 17:04:54 +0200

    game board css

--- public/script.js
+++ public/script.js
@@ -3,4 +3,6 @@ document.getElementById('new-game-btn').addEventListener('click',startgame)
 
 function startgame(){
     console.log('klikattu')
+    document . getElementById('intro-screen').style.display = 'none';
+    document . getElementById('game-screen').style.display = 'block';
 }
