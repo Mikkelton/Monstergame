151bb4605d339628f9c856025616aebd7cae63c0
Mikkelton <mikaelhakkinen7@gmail.com>
2025-02-19 16:59:42 +0200

    luotiin pelilauta

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const Board_Size = 15;
+let board;
 
 document.getElementById('new-game-btn').addEventListener('click',startgame)
 
@@ -5,4 +7,21 @@ function startgame(){
     console.log('klikattu')
     document . getElementById('intro-screen').style.display = 'none';
     document . getElementById('game-screen').style.display = 'block';
+
+    board = generateRandomBoard();
+}
+
+function generateRandomBoard(){
+    const newBoard = Array.from({length:Board_Size},()=>Array(Board_Size).fill(''));
+
+    for(let y = 0; y < Board_Size; y++){
+        for(let x = 0; x < Board_Size; x++){
+            if(y === 0){
+                newBoard[y][x] = 'W';
+            }
+        }
+    }
+
+    console.log(newBoard);
+    return newBoard;
 }
