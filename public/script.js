f5ddf2f5f7ab371a45c77ca8db983a57588d55ee
Mikkelton <mikaelhakkinen7@gmail.com>
2025-03-05 17:03:30 +0200

    luotiin pelilaudan rajat

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const Board_Size = 15;
+const cellSize = calculateCellSize();
 let board;
 
 document.getElementById('new-game-btn').addEventListener('click',startgame)
@@ -9,6 +10,8 @@ function startgame(){
     document . getElementById('game-screen').style.display = 'block';
 
     board = generateRandomBoard();
+
+    drawBoard(board);
 }
 
 function generateRandomBoard(){
@@ -22,6 +25,36 @@ function generateRandomBoard(){
         }
     }
 
+
     console.log(newBoard);
     return newBoard;
+}
+
+function drawBoard(board){
+    const gameBoard = document.getElementById('game-board');
+    gameBoard.style.gridTemplateColumns = `repeat(${Board_Size},1fr)`;
+
+    for(let y = 0; y < Board_Size; Y++){
+        for(let x = 0; x < Board_Size; x++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
+            if(getCell(board,x,y) === 'W'){
+                cell.classList.add('wall');
+            }
+
+            gameBoard.appendChild(cell);
+        }
+    }
+}
+
+function getCell(board,x,y){
+    return board[y][x];
+}
+
+function calculateCellSize(){
+    const screenSize = Math.min(window.innerWidth,window.innerHeight);
+    const gameBoardSize = 0.95 * screenSize;
+    return gameBoardSize / Board_Size;
 }
